81009229f37b530a8d5ff93111902782bf6691df
Sam Houghton <sam@3sidedcube.com>
2014-11-03 10:26:39 +0000

    Adds initial Xcode project

---
+++ StormLite/StormLite/StormLite.h
@@ -0,0 +1,13 @@
+//
+//  StormLite.h
+//  StormLite
+//
+//  Created by Sam Houghton on 03/11/2014.
+//  Copyright (c) 2014 Sam Houghton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StormLite : NSObject
+
+@end
---
+++ StormLite/StormLite/StormLite.m
@@ -0,0 +1,13 @@
+//
+//  StormLite.m
+//  StormLite
+//
+//  Created by Sam Houghton on 03/11/2014.
+//  Copyright (c) 2014 Sam Houghton. All rights reserved.
+//
+
+#import "StormLite.h"
+
+@implementation StormLite
+
+@end
